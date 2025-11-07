3165fdeaf190011a414a1681b0a13cbc2a10d71b
eacdc <ea@cdcprinters.com>
2025-11-07 16:32:39 +0530

    Add machine floor dashboard UI

--- MachineFloorDashboard/config.js
+++ MachineFloorDashboard/config.js
@@ -1,6 +1,6 @@
 window.AppConfig = window.AppConfig || {};
 
-window.AppConfig.apiBaseUrl = window.AppConfig.apiBaseUrl || 'http://localhost:3001/api';
+window.AppConfig.apiBaseUrl = window.AppConfig.apiBaseUrl || 'https://cdcapi.onrender.com/api';
 window.AppConfig.defaultMachineId = window.AppConfig.defaultMachineId || 58;
 window.AppConfig.defaultDatabase = window.AppConfig.defaultDatabase || 'KOL';
 window.AppConfig.refreshIntervalSeconds = window.AppConfig.refreshIntervalSeconds || 300;
